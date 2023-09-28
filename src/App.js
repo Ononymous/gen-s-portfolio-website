9be31d962b74ecb4ce71fa5091c90acb4a446d15
Gen Tamada <36137654+Ononymous@users.noreply.github.com>
2023-09-28 15:25:25 -0700

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar"
 import PhotoStack from "./PhotoStack"
 import About from "./About"
 import Footer from "./Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <PhotoStack/>
       <About/>
       <Footer/>
+      <Analytics />
     </div>
   );
 }
