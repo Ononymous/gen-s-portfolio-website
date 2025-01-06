92e6f9d9263ed43620c2de2a48e2eae7f18d3a82
Gen Tamada <gtamada@ucsb.edu>
2025-01-05 20:24:26 -0800

    research resume update

--- src/blocks/Now.js
+++ src/blocks/Now.js
@@ -19,15 +19,15 @@ function Now(){
                 <Text
                 as={'span'}
                 position={'relative'}>
-                    Education
+                    Research
                 </Text>
             </Heading>
             <Text fontSize={{ base: 'md', lg: 'lg' }}>
-                I am currently a 4th year Computer Science major at the University of California, Santa Barbara (UCSB), progressing through the BS/MS program with an expected completion in June 2026.
+                I am currently working in <Link color="teal.300" href="https://sites.cs.ucsb.edu/~ckrintz/racelab.html" isExternal>RACELab (The Lab for Research on Adaptive Computing Environments)</Link> at UCSB, directed by Professor Chandra Krintz and Professor Rich Wolski.
             </Text>
             <br/>
             <Text fontSize={{ base: 'md', lg: 'lg' }}>
-                I am taking Artificial Intelligence (CMPSC 165A) and Deep Learning (CMPSC 190I) this quarter quarter.
+                Our Lab focuses on research in the areas of cloud computing, edge computing, and IoT.
             </Text>
 
             <Heading
@@ -39,17 +39,16 @@ function Now(){
                 <Text
                 as={'span'}
                 position={'relative'}>
-                    Research
+                    Education
                 </Text>
             </Heading>
             <Text fontSize={{ base: 'md', lg: 'lg' }}>
-                I am currently working in <Link color="teal.300" href="https://sites.cs.ucsb.edu/~ckrintz/racelab.html" isExternal>RACELab (The Lab for Research on Adaptive Computing Environments)</Link> at UCSB, directed by Professor Chandra Krintz and Professor Rich Wolski.
+                I am currently a 4th year Computer Science major at the University of California, Santa Barbara (UCSB), progressing through the BS/MS program with an expected completion in June 2026.
             </Text>
             <br/>
             <Text fontSize={{ base: 'md', lg: 'lg' }}>
-                Our Lab focuses on research in the areas of cloud computing, edge computing, and IoT.
+                I am taking Runtime Systems (CMPSC 263) and Distributed Systems (CMPSC 271) this quarter.
             </Text>
-            {/* <br/> */}
 
             <Heading
             lineHeight={1.5}
@@ -64,32 +63,11 @@ function Now(){
                 </Text>
             </Heading>
             <Text fontSize={{ base: 'md', lg: 'lg' }}>
-                I was the Professional Development Head of UCSB Theta Tau Professional Engineering Fraternity, currently am the Rush Head for Fall 2024.
-            </Text>
-            <br/>
-            <Text fontSize={{ base: 'md', lg: 'lg' }}>
                 Have participated in the project series of UCSB Data Science Club, Coders SB, SB Hacks in the past, and have successfully risen up the ranks in each of them.
             </Text>
             <br/>
             <Text fontSize={{ base: 'md', lg: 'lg' }}>
-                Most recently, I have just finished my annual <Link color='teal.300' href="https://www.datascienceucsb.org/events" isExternal>UCSB Data Science Club</Link> presentation, got a 2nd place out of 50+ groups
-            </Text>
-            {/* <br/> */}
-
-            <Heading
-            lineHeight={1.5}
-            fontWeight={600}
-            marginTop={{xl:"7", md:"4", base:"2"}}
-            marginBottom={{xl:"7", md:"4", base:"2"}}
-            fontSize={{ base: '2xl', sm: '3xl', lg: '4xl' }}>
-                <Text
-                as={'span'}
-                position={'relative'}>
-                    Other Notes
-                </Text>
-            </Heading>
-            <Text fontSize={{ base: 'md', lg: 'lg' }}>
-                I am experienced with the Git, Wix, Wordpress, Cocos Creator, and Fusion 360 softwares and tools.
+                Attended previous Finalist Panel for the UCSB Data Science Club for supporting the club's growth and development.
             </Text>
         </Box>
     )
