a37c161443cde8b4fd0cb154852495c389d3ed63
Ononymous <gtamada@ucsb.edu>
2025-10-27 10:38:00 -0700

    new research

--- src/blocks/Now.tsx
+++ src/blocks/Now.tsx
@@ -18,6 +18,21 @@ const Now = () => {
         fontSize={{ base: '2xl', sm: '3xl', lg: '4xl' }}
       >
         <Text as="span" position="relative">
+          Research
+        </Text>
+      </Heading>
+      <Text fontSize={{ base: 'md', lg: 'lg' }}>
+        Currently working in UCSB ArchLab under Professor Balkind on research related to hardware acceleration, hardware network stack, and FPGA as a whole.
+      </Text>
+
+      <Heading
+        lineHeight={1.5}
+        fontWeight={600}
+        marginTop={{ xl: '7', md: '4', base: '2' }}
+        marginBottom={{ xl: '7', md: '4', base: '2' }}
+        fontSize={{ base: '2xl', sm: '3xl', lg: '4xl' }}
+      >
+        <Text as="span" position="relative">
           Education
         </Text>
       </Heading>
